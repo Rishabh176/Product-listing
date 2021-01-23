d2709cb0175c1e091a0e7f4d8f4d1bddbb695040
Rishabh176 <rishabhemail8@gmail.com>
2021-01-24 00:14:36 +0530

    installing libraries and setting folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import  {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './App.css';
+import List from './containers/List'
+// import Product from './containers/Product'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={List}/>
+          {/* <Route path='/login' component={Login} /> */}
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
